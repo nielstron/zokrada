4372407989de951799e032b2e6ee5f5c8205a897
Niels Mündler <n.muendler@posteo.de>
2023-06-02 23:18:43 +0200

    Express verification in terms of pairing

--- groth16/verifier.py
+++ groth16/verifier.py
@@ -9,7 +9,20 @@ def verify(verifying_key: VerifyingKey, input: List[int], proof: Proof) -> bool:
     assert len(input) + 1 == len(verifying_key.gamma_abc), "Length of verifying key and input are incompatible"
     # Compute the linear comnbination vk_x
     vk_x = G1Point(0, 0)
-    return False
+    for j, i in zip(vk.gamma_abc[1:], input):
+        assert i < snark_scalar_field, "Input value too large"
+        vk_x = pairing_addition(vk_x, pairing_scalar_mul(j, i))
+    vk_x = pairing_addition(vk_x, vk.gamma_abc[0])
+    return not pairing_prod_4(
+        proof.a,
+        proof.b,
+        pairing_negate(vk_x),
+        vk.gamma,
+        pairing_negate(proof.c),
+        vk.delta,
+        pairing_negate(vk.alpha),
+        vk.beta
+    )
 
 
 # the cardano validator for groth16 zksnarks onchain verifier is a parameterized script
