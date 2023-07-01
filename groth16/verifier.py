058d844036345e3fcf9dfcd2dc32f3d6a17c0a7a
Niels Mündler <n.muendler@posteo.de>
2023-07-01 10:00:23 +0200

    Make the params datum

--- groth16/verifier.py
+++ groth16/verifier.py
@@ -29,14 +29,22 @@ def verify(verifying_key: VerifyingKey, input: List[int], proof: Proof) -> bool:
         vk.beta,
     )
 
+@dataclass
+class ChallengeParameters(PlutusData):
+    verifying_key: VerifyingKey
+    input_length: int
+
+@dataclass
+class ProofParameters(PlutusData):
+    proof: Proof
+    input: List[int]
+
 
 # the cardano validator for groth16 zksnarks onchain verifier is a parameterized script
 def validator(
-    verifying_key: VerifyingKey,
-    input_length: int,
-    proof: Proof,
-    input: List[int],
+    datum: ChallengeParameters,
+    redeemer: ProofParameters,
     context: ScriptContext,
 ) -> None:
-    assert len(input) == input_length, "Length of input must be " + str(input_length)
-    assert verify(verifying_key, input, proof), "Proof could not be verified"
+    assert len(redeemer.input) == datum.input_length, "Length of input must be " + str(datum.input_length)
+    assert verify(datum.verifying_key, redeemer.input, redeemer.proof), "Proof could not be verified"
